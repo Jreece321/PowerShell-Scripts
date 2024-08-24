84db8a680e5f6ed678aed125b97bcd1d7d5f6fdb
Jreece321 <123501852+Jreece321@users.noreply.github.com>
2024-08-24 01:27:42 -0400

    Update mdt_install_test.ps1
    Added comments for documentation as well as details about the  synopsis, parameters, and examples.

--- mdt_install_test.ps1
+++ mdt_install_test.ps1
@@ -1,3 +1,33 @@
+<#
+.SYNOPSIS 
+Script to remotely execute application installs via MDT Server on designated PCs. Includes interactive menu to select an application for install. 
+
+.PARAMETER list 
+(-l) Input file name that contains list of desired PCs
+
+.PARAMETER room_method 
+(-r) Command line switch to prompt user for entire rooms of PCs to remote install apps on
+
+.EXAMPLE 
+.\mdt-install.ps1 -r 
+
+Runs the remote install script and prompts the user for room number input for application installs to entire rooms at a time
+
+Use Case: Easier if you know you have to do full room installs beforehand 
+
+.\mdt-intall.ps1 -l <relative or full path to .txt file containing PC list>
+
+Runs the remote install script using a txt file to retrieve target PCs from
+
+Use Case: Useful if you don't have to do entire rooms. Potentially partial rooms, or PCs scattered throughout multiple rooms.
+
+
+
+#>
+
+
+
+
 param (
 
  [Parameter(Mandatory=$false)][string]$list,
@@ -6,11 +36,10 @@ param (
 
 function Start-Menu {
 
-    $apps = Get-ChildItem -Path C:\Users\jeree\script_test | Select-Object -exp Name 
-    $app_length = $apps.Length -1
+    $apps = Get-ChildItem -Path C:\Users\jeree\script_test | Select-Object -exp Name  
     $apps_hashtable = @{}
      
-    for ($x = 0; $x -lt $apps.Length; $x++){
+    for ($x = 0; $x -lt $apps.Length; $x++){ # loop to iterate through directories and assign them to a hashtable for use in interactive menu
    
        $apps_hashtable.Add("$x", $apps[$x])
    
@@ -18,19 +47,19 @@ function Start-Menu {
 
     While ($true){
 
-    $apps_hashtable.GetEnumerator() | Sort-Object  Value |  Format-Table 
+    $apps_hashtable.GetEnumerator() | Sort-Object  Value |  Format-Table # calling on the getenumerator function to show all application directories to the screen
     Write-host "[s] Search for Application"
     Write-host "[0 - $app_length] Install Corresponding Applicaiton" 
     Write-host "[q] Exit this Script"
     $search = read-host 
 
-    if ($search -like "s"){
+    if ($search -like "s"){ # If input is "s", initiates code block to handle searches
 
         $search = read-host "Please enter the name of the application you'd like to search"
         $global:result = $apps_hashtable.GetEnumerator() | Where-Object Value -like *$search* | Sort-Object Name | Format-Table  
         
-        if ( $null -eq $global:result ){
-            Write-host "There were no matches with your search, Please try again"
+        if ( $null -eq $global:result ){ # Restarts to main menu if no partial matches 
+            Write-host "There were no partial matches with your search, Please try again"
             Write-host ""
             Write-Host "Returning to Beginning of Menu..."
             Start-Sleep -Seconds 3 
@@ -38,6 +67,7 @@ function Start-Menu {
         }
 
         Write-host "Below are the applications that are similar to your search."
+        Write-host ""
         $global:result  
 
         $choice = read-host "Select a number to install or press q to restart"
@@ -80,9 +110,9 @@ function Start-Menu {
         exit 1 
     }
 
-    elseif ($search -gt $apps.Length){
+    elseif (($search -gt $apps.Length) -or ($search -lt 0)){
 
-        Write-Host"the value you input is too high, please try again"
+        Write-Host"the value you input is invalid, please try again"
         Start-sleep -seconds 3 
     }
     }
@@ -91,24 +121,24 @@ function Start-Menu {
 
 function remote-install {
 
-    Invoke-Command -ComputerName $args[0] {
+    Invoke-Command -ComputerName $args[0] { # Invokes commands on remote computer given a list of PCs designated by the -ComputerName parameter
     
-        Invoke-Expression $net_command
+        Invoke-Expression $using:net_command 
     
-        cd "Z:\Applications\$result"
+        cd "Z:\Applications\$using:result" # Change directory into designated applicaiton folder 
     
-        $cmd_file = Get-ChildItem -path "Z:\Applications\$result" | Where-Object Name -like "*.cmd" | select -exp Name
+        $cmd_file = Get-ChildItem -path "Z:\Applications\$using:result" | Where-Object Name -like "*.cmd" | select -exp Name # Extracts the .cmd file from app directory
         
-        Invoke-Expression .\$cmd_file 
+        Invoke-Expression .\$cmd_file # executes .cmd file on remote computer
     
         }
     
     }
 
-function pc-health {
+function pc-health { # If PC is able to be pinged, add it to alive_pcs array. If not, display error message to screen
 
     $check = $args[0]
-    If ( Test-Connection -BufferSize 32 -Count 1 -ComputerName $check -Quiet )
+    If ( Test-Connection -BufferSize 32 -Count 1 -ComputerName $check -Quiet )  
     { 
         $alive_pcs += $check 
     }
@@ -117,30 +147,34 @@ function pc-health {
          continue }
          
     }
-function get-pclist {
+function get-pclist { # Function to take room number input and output list of PCs
 
-    do {
-        $room = Read-Host "Please enter your room number(s). Press q and enter when finished"
+    do { # Loop to keep prompting users for rooms until they press "q" 
+        $room = Read-Host "Please enter your room number(s). (i.e openlab or a227). Press q and enter when finished" 
             
-            if ($room -notlike "q"){
+            if ($room -notlike "q"){ # Logic to make sure that the letter "q" is not entered into the array
             
-            $roomarray += $room
+            $roomarray += $room # adds user-defined room to room array 
                 
             }
-        } until ($room -like "q")
+        } until ($room -like "q") # if user input is "q", exits the loop and continues to making the PC list 
             
               
-    foreach ($room in $roomarray){
+    foreach ($room in $roomarray){ # Iterates through room array to change dns formatting and conduct AD search 
             
             
-        $length =$room.Length 
-        $room = $room.Insert($length,"-*")
-        $room = $room.Insert(0,"s-")
+        $length =$room.Length # assigns the length of the room string to a variable
+        $room = $room.Insert($length,"-*") # adds a wildcard asterisk at the end of the string to search for all PCs beginning with that DNS
+        $room = $room.Insert(0,"s-") # adds correct Stanton Campus dns prefix 
+
+        # Example: Input: openlab Output: s-openlab-*
                 
-        $getad = (([adsisearcher]"(&(objectCategory=Computer)(name=$room))").findall()).properties
+        $getad = (([adsisearcher]"(&(objectCategory=Computer)(name=$room))").findall()).properties # Conducts an AD search based on Computer objects whose name is like room dns
                     
-        $pc_list += $getad.cn
+        $pc_list += $getad.cn # Adds all PCs dns names (that start with the room number) to pc_list array 
             
+        #Example:
+        # $room = s-openlab-*. Output would be s-openlab-01, s-openlab-02.... s-openlab-14, etc. 
         }
     
     }
@@ -191,10 +225,10 @@ if ($room_method){
 }
 elseif ($list){
 
-$pc_list = get-content $list # assigns $pc_list to the content of the list provided in the -l parameter
+$pc_list = get-content $list # if list was provided, assigns $pc_list to the content of the list provided in the -l parameter
 }
 
-pc-health $pc_list # Calls pc health function to test connection on the pc_list; the ones that are connected get assigned to the alive_pcs array
+pc-health $pc_list # Calls pc health function to test connection on the pc_list; the ones that are able to be pinged get assigned to the alive_pcs array
 
 remote-install $alive_pcs # calls remote-install function on the alive_pcs array. Uses invoke-command to run the remote installs in parallel with eachother
 
